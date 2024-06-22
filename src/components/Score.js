2f6aef2b2be87358ace1dd3be414e5f773831d51
mariem <mariamkhaled709@gmail.com>
2024-06-22 06:05:27 +0300

    update-design

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,12 +1,14 @@
 export default function Score (props){
     return(
+        <>
         <div className="score-con">
          <span>dice rolled:{props.rolls}</span>
                 <br></br>
-        <span>Timer: {("0"+Math.floor((props.time /6000)%60)).slice(-2)}:
+                <span>Timer:</span>
+        <span> {("0"+Math.floor((props.time /6000)%60)).slice(-2)}:
             {("0"+Math.floor((props.time/1000)%60)).slice(-2)}:
             {("0"+Math.floor((props.time/10)%1000)).slice(-2)}
             </span>
         </div>
-    )
+    </>)
 }
